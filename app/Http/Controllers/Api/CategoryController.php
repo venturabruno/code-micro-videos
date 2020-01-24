445fa1a3c95efb6d45d4655ac26feab22affe8c6
Bruno <bruno@MacBook-Pro-de-Bruno.local>
2020-01-23 21:17:59 -0300

    Inicio do projeto

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return Category::all();
+    }
+
+    public function store(CategoryRequest $request)
+    {
+        return Category::create($request->all());
+    }
+
+    public function show(Category $category)
+    {
+        return $category;
+    }
+
+    public function update(CategoryRequest $request, Category $category)
+    {
+        $category->update($request->all());
+        return $category;
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->noContent();
+    }
+}
